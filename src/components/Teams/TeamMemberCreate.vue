77b2cea0cb301aa250086a4ae0db49ad483748ef
Shoaib Rehan <ranashoaibrehan@gmail.com>
2024-01-02 00:18:51 +0500

    changes in modal

--- src/components/Teams/TeamMemberCreate.vue
+++ src/components/Teams/TeamMemberCreate.vue
@@ -1,8 +1,10 @@
 <script setup>
+import {useFlash} from '@/composables/useFlash';
 import { useTeamStore } from '@/stores/TeamStore';
 import Modal from '@/components/Modal.vue';
 import { ref } from 'vue';
 
+let { flash } = useFlash();
 let team = useTeamStore();
 let showModal = ref(false);
 
@@ -12,16 +14,22 @@ let status = ref('Active');
 
 function addMember() {
     team.members.push({
-        name: name,
-        email: email,
-        status: status
+        name: name.value,
+        email: email.value,
+        status: status.value
     });
+
+    name.value = '';
+    email.value = '';
+    status.value = 'Active';
+
+    flash('Success', 'Member created successfully', 'success');
 }
 </script>
 
 <template>
     <button 
-        class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+        class="px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600 disabled:bg-gray-400"
         :disabled="!team.spotsRemaining"
         @click="showModal = true"
     >
@@ -37,12 +45,23 @@ function addMember() {
             <template #default>
                 <p>Add a member to your team.</p>
                 <form action="" class="mt-4">
-                    <div class="grid gap-2 justify-between">
-                        <input type="text" v-model="name" placeholder="Enter name..." />
-                        <input type="email" v-model="email" placeholder="Email address..." />
-                        <input type="text" v-model="status" placeholder="Status..." />
+                    <div class="grid justify-between gap-2">
+                        <input type="text" v-model="name" placeholder="Enter name..." class="p-2 mt-3 border w-96" />
+                        <input type="email" v-model="email" placeholder="Email address..." class="p-2 mt-3 border" />
+                        <input type="text" v-model="status" placeholder="Status..." class="p-2 mt-3 border" />
 
-                        <button @click.prevent="addMember">Add</button>
+                        <div class="flex gap-5 mt-4">
+                            <div class="flex gap-2">
+                                <input type="radio" id="one" value="Active" v-model="status" />
+                                <label for="one">Active</label>
+                            </div>
+                            <div class="flex gap-2">
+                                <input type="radio" id="two" value="Blocked" v-model="status" />
+                                <label for="two">Blocked</label>
+                            </div>
+                        </div>
+
+                        <button @click.prevent="addMember" class="p-2 mt-4 bg-green-400 border rounded hover:bg-green-500 rounded-xl">Add</button>
                     </div>
                 </form>
             </template>
